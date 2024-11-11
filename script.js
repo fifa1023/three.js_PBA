491332237ba63d621fc3b65e22bd4668bf7cac8c
yuk. <yuki.shimizu@speee.jp>
2024-11-11 17:29:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // シーン、カメラ、レンダラーの設定
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 200; // カメラを遠くに配置して多くの星が見えるように
+camera.position.z = 400; // カメラを遠くに配置して多くの星が見えるように
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
